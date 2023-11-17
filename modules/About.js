e8c1c2cd677763fa1243ef74b9bd5cad4834f2e2
raha-sajed <raha.sajed.82@gmail.com>
2023-11-17 17:16:40 +0330

    h4

--- modules/About.js
+++ modules/About.js
@@ -1,7 +1,6 @@
 const About = async () => {
     let data = await fetch("https://raha-sajed.github.io/json-golestan/db.json");
     let res = await data.json();
-    console.log(res);
     let about = res.about.map((elem) => {
         return `<h4>${elem.title}</h4>
         <p>${elem.p}
